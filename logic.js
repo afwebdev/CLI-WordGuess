3608398b3d245037751499638175f71073ac350e
AFWebDev <andrewfariaj@gmail.com>
2019-09-03 21:24:07 -0400

    ..

--- logic.js
+++ logic.js
@@ -1,10 +1,11 @@
 let inquirer = require("inquirer");
+//massive word list package, returns a JSON of words, words in 50,000-100,xxx range are best.
 let wordList = require("word-list-json");
 let Letter = require("./Letter");
 let Word = require("./Word.js");
 
-let word = new Word();
-word.storeWord(wordList[`${Math.floor(Math.random() * 50000) + 60000}`]);
+let word = new Word(wordList[`${Math.floor(Math.random() * 50000) + 50000}`]);
+console.log(word.word);
 
 const askForLetter = () => {
 	inquirer
